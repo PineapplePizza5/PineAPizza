c628b80ea9f81c4fb2dfb50b5240f24706e71ca9
최은정 <morned270@daum.net>
2019-11-27 12:11:48 +0900

    changed table scene

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,15 @@
+﻿//작성자 : 최은정 (류서현, 신은지, 이송이, 최세화, 최은정, 홍예지)
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour { 
+    public void SceneLoad(string sceneName) {   //씬 이름(문자열)을 통해 씬을 로드
+        SceneManager.LoadScene(sceneName);
+    }
+    public void SceneLoad(int sceneNum) {   //씬 번호(숫자)를 통해 씬을 로드
+        SceneManager.LoadScene(sceneNum);
+    }
+}

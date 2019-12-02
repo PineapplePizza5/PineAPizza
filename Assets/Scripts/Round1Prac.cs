22f86ddaba814be087920f78c4a87eb46861bc29
최은정 <morned270@daum.net>
2019-12-03 07:02:23 +0900

    added scripts of TableScene and OvenScene

---
+++ Assets/Scripts/Round1Prac.cs
@@ -0,0 +1,101 @@
+﻿/* ***************************************************************
+ * 프로그램 명 : Round1Prac.cs
+ * 작성자 : 최은정 (이송이, 류서현, 신은지, 최세화, 홍예지)
+ * 최조 작성일 : 2019년 12월 02일
+ * 최종 작성일 : 2019년 12월 03일
+ * 프로그램 설명 : Round1의 연습 단계에 알맞게 창을 구성한다.
+ * *************************************************************** */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Round1Prac : MonoBehaviour
+{
+    public GameObject pineApple;
+    public GameObject sliced;
+    public GameObject rectangle;
+    public GameObject movement1;
+    public GameObject movement2;
+    public GameObject correct;
+    public GameObject wrong;
+
+    int a = 1;  //a가 1이면 동작을 첫 번째로 보여준다는 것을 의미
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (true)   //고앵이의 상황 변수가 라운드1의 연습일 때 실행
+        {
+            Invoke("Round1prac", 8);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Round1prac()
+    {
+        rectangle.SetActive(true);
+        movement1.SetActive(false);
+        movement2.SetActive(false);
+        sliced.SetActive(false);
+        Invoke("ShowMovement1", 1);
+    }
+
+    void ShowMovement1()
+    {
+        movement1.SetActive(true);
+        Invoke("ShowMovement2", 1);
+    }
+
+    void ShowMovement2()
+    {
+        movement1.SetActive(false);
+        movement2.SetActive(true);
+
+        if (a == 1)
+        {
+            a--;
+            Invoke("Round1prac", 3);
+        }
+        else
+        {
+            Invoke("ShowPineApple", 2);
+        }
+        
+    }
+
+    void ShowPineApple()
+    {
+        wrong.SetActive(false);
+        rectangle.SetActive(false);
+        movement2.SetActive(false);
+        pineApple.SetActive(true);
+
+        if (true)   //'재료 손질' 동작이 알맞게 인식된 경우(수정 필요)
+        {
+            correct.SetActive(true);    //동그라미 표시
+            Invoke("ShowSliced", 2);    //손질된 재료를 보여주는 함수 호출
+        }
+        else        //틀린 동작이 인식된 경우
+        {
+            wrong.SetActive(true);      //엑스 표시
+            Invoke("ShowPineApple", 2); //ShowPineApple 재귀호출
+        }
+    }
+
+    void ShowSliced()
+    {
+        rectangle.SetActive(false);
+        movement2.SetActive(false);
+        pineApple.SetActive(false);
+        correct.SetActive(false);
+        sliced.SetActive(true);        
+
+    }
+
+}

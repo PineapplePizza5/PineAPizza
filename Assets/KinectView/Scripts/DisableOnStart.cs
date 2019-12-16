88533c11542c2453d3661541e3e38f0791ac72eb
choisaywhy <43446451+choisaywhy@users.noreply.github.com>
2019-12-16 14:27:04 +0900

    add_comment

--- Assets/KinectView/Scripts/DisableOnStart.cs
+++ Assets/KinectView/Scripts/DisableOnStart.cs
@@ -1,4 +1,12 @@
-﻿using UnityEngine;
+  
+/* ***************************************************************
+ * 프로그램 명 : DisableOnStart.cs
+ * 작성자 : 신은지, 최세화 (류서현, 이송이, 최은정, 홍예지) + 오픈소스 - kinect for unity
+ * 최조 작성일 : 2019년 11월 14일
+ * 최종 작성일 : 2019년 12월 15일
+ * 프로그램 설명 : 시작할 때 게임 오브젝트가 보이지 않도록 비활성화 하게 만든다 
+ * *************************************************************** */
+using UnityEngine;
 using System.Collections;
 
 public class DisableOnStart : MonoBehaviour {

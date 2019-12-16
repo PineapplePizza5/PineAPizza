d207bf8ab0525c6ac6d9b0e9af3f35d8a17fc854
choisaywhy <43446451+choisaywhy@users.noreply.github.com>
2019-12-16 14:31:35 +0900

    add_comment

--- Assets/KinectView/Scripts/MultiSourceManager.cs
+++ Assets/KinectView/Scripts/MultiSourceManager.cs
@@ -1,4 +1,11 @@
-﻿using UnityEngine;
+/* ***************************************************************
+ * 프로그램 명 : MultiSourceManager.cs
+ * 작성자 : 신은지, 최세화 (류서현, 이송이, 최은정, 홍예지) + 오픈소스 - kinect for unity
+ * 최조 작성일 : 2019년 11월 14일
+ * 최종 작성일 : 2019년 12월 15일
+ * 프로그램 설명 : color, depth, infrared 기능들을 전부 합쳐와 전체적인 키넥트 인식 기능을 수행하도록 한다.
+ * *************************************************************** */
+using UnityEngine;
 using System.Collections;
 using Windows.Kinect;
 

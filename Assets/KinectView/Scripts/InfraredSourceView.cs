886508ddd49549d8a66d7fb89027a13bbd350068
choisaywhy <43446451+choisaywhy@users.noreply.github.com>
2019-12-16 14:32:02 +0900

    add_comment

--- Assets/KinectView/Scripts/InfraredSourceView.cs
+++ Assets/KinectView/Scripts/InfraredSourceView.cs
@@ -7,6 +7,7 @@
  * *************************************************************** */
 using UnityEngine;
 using System.Collections;
+using Windows.Kinect;
 
 public class InfraredSourceView : MonoBehaviour 
 {

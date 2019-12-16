9ca1165e1341be01afc0bbfbbd42dd1dd68ead22
choisaywhy <43446451+choisaywhy@users.noreply.github.com>
2019-12-16 14:24:03 +0900

    add_comment

--- Assets/KinectView/Scripts/ColorSourceView.cs
+++ Assets/KinectView/Scripts/ColorSourceView.cs
@@ -1,4 +1,11 @@
-﻿using UnityEngine;
+/* ***************************************************************
+ * 프로그램 명 : ColorSourceView.cs
+ * 작성자 : 신은지, 최세화 (류서현, 이송이, 최은정, 홍예지) + 오픈소스 - kinect for unity
+ * 최조 작성일 : 2019년 11월 14일
+ * 최종 작성일 : 2019년 12월 15일
+ * 프로그램 설명 : ColorSourceManager를 이용, 게임 오브젝트 컴포넌트를 설정한다.
+ * *************************************************************** */
+using UnityEngine;
 using System.Collections;
 using Windows.Kinect;
 
